063336416af2f9cb7be7bb0fdd91792b91a1bc89
MishkaV <mvorozhtsov.work@gmail.com>
2026-02-05 11:39:05 +0100

    Add base UI for profile

--- features/profile/impl/build.gradle.kts
+++ features/profile/impl/build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     alias(libs.plugins.voyager.feature.impl)
+    alias(libs.plugins.voyager.compose.resources)
+    alias(libs.plugins.voyager.secrets)
 }
 
 kotlin {
     sourceSets {
         commonMain.dependencies {
             api(projects.features.profile.api)
+
+            implementation(projects.features.main.api)
         }
     }
 }
--- features/profile/impl/src/commonMain/kotlin/io/mishka/voyager/profile/impl/ui/ProfileScreen.kt
+++ features/profile/impl/src/commonMain/kotlin/io/mishka/voyager/profile/impl/ui/ProfileScreen.kt
@@ -1,11 +1,131 @@
 package io.mishka.voyager.profile.impl.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import io.mishka.voyager.features.main.api.consts.MAIN_BOTTOM_BAR_HEIGHT
+import io.mishkav.voyager.core.ui.theme.VoyagerTheme
+import io.mishkav.voyager.core.ui.uikit.button.VoyagerButton
+import io.mishkav.voyager.core.ui.uikit.button.VoyagerDefaultButtonSizes
+import io.mishkav.voyager.core.ui.uikit.button.VoyagerDefaultButtonStyles
+import io.mishkav.voyager.core.ui.uikit.snackbar.compose.noOverlapBottomContentBySnackbar
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import voyager.features.profile.impl.BuildKonfig
+import voyager.features.profile.impl.generated.resources.Res
+import voyager.features.profile.impl.generated.resources.avatar
+import voyager.features.profile.impl.generated.resources.profile_app_version
+import voyager.features.profile.impl.generated.resources.profile_button_email_us
+import voyager.features.profile.impl.generated.resources.profile_button_sign_out
+import voyager.features.profile.impl.generated.resources.profile_tab_title
+import voyager.features.profile.impl.generated.resources.profile_title
 
 @Composable
 fun ProfileScreen(
     viewModel: ProfileViewModel,
     modifier: Modifier = Modifier,
 ) {
+    ProfileScreenContent(
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun ProfileScreenContent(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .background(VoyagerTheme.colors.background)
+            .windowInsetsPadding(WindowInsets.systemBars)
+            .padding(bottom = MAIN_BOTTOM_BAR_HEIGHT + 12.dp, top = 16.dp)
+            .padding(horizontal = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(Res.string.profile_tab_title),
+            style = VoyagerTheme.typography.h1,
+            textAlign = TextAlign.Start,
+            color = VoyagerTheme.colors.font
+        )
+
+        Spacer(Modifier.height(38.dp))
+
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(0.52f)
+                .aspectRatio(1f)
+                .clip(CircleShape),
+            painter = painterResource(Res.drawable.avatar),
+            contentDescription = null,
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(Res.string.profile_title),
+            style = VoyagerTheme.typography.h2,
+            textAlign = TextAlign.Center,
+            color = VoyagerTheme.colors.font
+        )
+
+        Spacer(Modifier.height(24.dp))
+
+        // TODO Stats
+
+        Spacer(Modifier.weight(1f))
+
+        VoyagerButton(
+            modifier = Modifier.fillMaxWidth(),
+            style = VoyagerDefaultButtonStyles.custom(
+                containerColor = VoyagerTheme.colors.border,
+                contentColor = VoyagerTheme.colors.font,
+            ),
+            size = VoyagerDefaultButtonSizes.buttonXL(),
+            text = stringResource(Res.string.profile_button_email_us),
+            onClick = {
+                // TODO
+            }
+        )
+
+        Spacer(Modifier.height(12.dp))
+
+        VoyagerButton(
+            modifier = Modifier.fillMaxWidth(),
+            style = VoyagerDefaultButtonStyles.primary(),
+            size = VoyagerDefaultButtonSizes.buttonXL(),
+            text = stringResource(Res.string.profile_button_sign_out),
+            onClick = {
+                // TODO
+            }
+        )
+
+        Spacer(Modifier.height(8.dp))
+
+        Text(
+            text = stringResource(Res.string.profile_app_version, BuildKonfig.VERSION_NAME),
+            style = VoyagerTheme.typography.body,
+            textAlign = TextAlign.Center,
+            color = VoyagerTheme.colors.subtext
+        )
+    }
 }
